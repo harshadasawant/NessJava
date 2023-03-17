8bde81d96d6c14563f442d417884018f6acd3372
harshada <harshadasawant22@gmail.com>
2023-03-17 12:07:11 +0530

    Inheritance with abstract class

---
+++ src/Inheritance.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+abstract class SuperClass{
+    public  void display() {
+        System.out.println("from super class");
+    }
+    abstract void disp();
+
+}
+public class Inheritance extends SuperClass{
+
+   public void display()   {
+        System.out.println("from sub class");
+    }
+
+    @Override
+    void disp() {
+        System.out.println(" calling from sub class");
+    }
+
+    public static void main(String[] args) {
+        SuperClass superClass = new Inheritance();
+
+        SuperClass inheritance = new Inheritance();
+        Inheritance inheritance1 = new Inheritance();
+
+//        superClass.disp();
+//        inheritance.disp(); can not call because reference is a super class can access only super class method
+        inheritance.disp();
+        System.out.println("================");
+        inheritance.display();
+        inheritance1.disp();
+
+
+        superClass.display();
+        inheritance1.display();
+
+    }
+}
