c938fd1c70ce0ead73dc4009174c27dec388b178
harshadasawant22@gmail.com <Panasonic@1209>
2023-03-24 12:04:47 +0530

    File Reader with FileReader

--- src/FileInputExample.java
+++ src/FileInputExample.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FileInputExample {
@@ -10,14 +11,32 @@ public class FileInputExample {
             while ((i=fin.read())!=-1) {
                 System.out.print((char) i);
             }
+            System.out.println();
             fin.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    static void fileReader(){
+        try {
+            FileReader fr = new FileReader("testWriter.txt");
+            int i =0;
+
+            while((i=fr.read())!=-1)
+            {
+                System.out.print((char) i);
+            }
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) {
         FileInputExample fi = new FileInputExample();
         fi.fileInputRead();
+        fileReader();
     }
 }
