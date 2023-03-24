0c27ad412398b9eb98405ee57677265f3ec32f25
harshadasawant22@gmail.com <Panasonic@1209>
2023-03-24 09:40:36 +0530

    Functional interface or lambda function

--- src/InterfaceExample.java
+++ src/InterfaceExample.java
@@ -1,22 +1,42 @@
+@FunctionalInterface
+interface FI {
+    void add(int a);
 
-interface  FI{
-    void add();
+    default void add1() {
+        System.out.println("from default add method");
+    }
+
 }
-public class InterfaceExample implements FI{
-    public void add(){
+
+public class InterfaceExample implements FI {
+    public void add(int a) {
         System.out.println("add");
     }
 
+
     public static void main(String[] args) {
         FI obj = new InterfaceExample();
-        obj.add();
+        obj.add(2);
         InterfaceExample obj1 = new InterfaceExample();
-        obj1.add();
+        obj1.add(2);
     }
 }
 
-class DifferentClass{
+class DifferentClass {
     public static void main(String[] args) {
+        FI obj = new FI() {
+            public void add(int a) {
+                System.out.println("from add method");
+            }
+        };
+        obj.add(4);
+
+        FI obj1 = (a) -> {
+            System.out.println("from add method lambda == " + a);
+        };
+
+        obj1.add(3);
+
 
     }
 }
