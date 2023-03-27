30e515bde61ee78c52ab914d2b5c9c8999d7074c
harshadasawant22@gmail.com <Panasonic@1209>
2023-03-27 11:19:40 +0530

    Sleep method and join method

--- src/ThreadClassExample.java
+++ src/ThreadClassExample.java
@@ -1,10 +1,16 @@
 public class ThreadClassExample extends Thread {
+  ThreadClassExample(){
+
+  }
+  ThreadClassExample(String name){
+    super(name);
+  }
   public void run(){
     System.out.println("running from run method   "+Thread.currentThread().getName());
   }
 
   public static void main(String[] args) {
-    ThreadClassExample threadObj = new ThreadClassExample();
+    ThreadClassExample threadObj = new ThreadClassExample("My Thread");
     threadObj.start();
     System.out.println(Thread.currentThread().getName());
   }
