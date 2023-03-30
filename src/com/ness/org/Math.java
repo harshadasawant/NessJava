7ef2a519d311b3821b937cb7172e0cdf67b97234
harshada <harshadasawant22@gmail.com>
2023-03-30 09:30:26 +0530

    Junit class

---
+++ src/com/ness/org/Math.java
@@ -0,0 +1,7 @@
+package com.ness.org;
+//ctrl+shift+t
+public class Math {
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ test/com/ness/org/MathTest.java
@@ -0,0 +1,12 @@
+package com.ness.org;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathTest {
+
+    @Test
+    void add() {
+    }
+}
