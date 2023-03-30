095c95013468276047faeda8ebc27f9c539a68bd
harshada <harshadasawant22@gmail.com>
2023-03-30 10:05:45 +0530

    testing add and sub method

--- test/com/ness/org/MathTest.java
+++ test/com/ness/org/MathTest.java
@@ -4,9 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//Expected value -  Assuming
+//Actual Value - what u r code return
+//TDD
 class MathTest {
 
     @Test
-    void add() {
+    void testAdd() {
+        Math obj = new Math();
+        int answer = obj.add(2, 3);
+        System.out.println("hello");
+        assertEquals(5, answer);
+    }
+
+    @Test
+    void testSub() {
+        System.out.println("hello");
+        Math obj  = new Math();
+        int ans = obj.sub(5,3);
+        assertEquals(2,ans);
+
     }
 }
